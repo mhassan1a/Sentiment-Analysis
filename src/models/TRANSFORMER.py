0cb184b99c56363fc5c2dd29d81d71d3bb958074
Mohamed Hassan <mmabdelkader@outlook.com>
2024-06-11 08:39:40 +0200

    add positional encoding to transformer model

--- src/models/TRANSFORMER.py
+++ src/models/TRANSFORMER.py
@@ -1,12 +1,41 @@
 import torch
 import torch.nn as nn
 from transformers import BertTokenizer, BertModel
-#
+import math
+from torch import Tensor
+
+
+class PositionalEncoding(nn.Module):
+    """ Positional encoding for transformer model
+    https://stackoverflow.com/questions/77444485/using-positional-encoding-in-pytorch
+
+    """
+    
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+
 class TransformerClassifier(nn.Module):
     def __init__(self, input_dim, embedding_dim, 
-                 num_classes, n_heads, n_layers, dropout, trans_feedforward, embedding_weights=None):
+                 num_classes, n_heads, n_layers, dropout, 
+                 trans_feedforward, embedding_weights=None):
         super(TransformerClassifier, self).__init__()
-    
         self.embedding = nn.Embedding(input_dim, embedding_dim)
         if embedding_weights is not None:
             self.embedding.weight = nn.Parameter(embedding_weights, requires_grad=False)
@@ -21,7 +50,7 @@ class TransformerClassifier(nn.Module):
         self.fc = nn.Linear(embedding_dim, num_classes, bias=False)
 
     def forward(self, x, mask):
-        embedded = self.embedding(x) 
+        embedded = self.embedding(x) + PositionalEncoding(embedding_dim)(self.embedding(x))
         embedded = embedded.permute(1, 0, 2)  
         transformer_out = self.transformer_encoder(embedded)
         out = transformer_out.mean(dim=0)  
